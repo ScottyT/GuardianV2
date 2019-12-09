c4d80ebe43119e6fecfc7d9d2355325f2347db96
Scott Tucker <stuck04@gmail.com>
2019-12-09 14:18:12 -0500

    ssfsdfsdf

--- src/views/Services.vue
+++ src/views/Services.vue
@@ -337,9 +337,8 @@ export default {
       @include respond(mobileSmall) {
         opacity: 0;
         height: 1px;
-        // -webkit-transform: translateY(-154px);
-        // transform: translateY(-154px);
-        -webkit-transform: scale(1);
+        -webkit-transform: translateY(-154px);
+        transform: translateY(-154px);
         -webkit-transition: all 0.3s ease-in-out;
         transition: all 0.3s ease-in-out;
       }
@@ -381,10 +380,11 @@ export default {
         @include respond(mobileLarge) {
           height: 190px;
           opacity: 1;
-          -webkit-transform: scale(1);
-          -webkit-animation-name: slideDownDesktop;
-          -webkit-animation-duration: 0.6s;
-          -webkit-animation-timing-function: ease-in;
+          -webkit-transform: translateY(0);
+          transition: all 0.3s ease-in;
+          // -webkit-animation-name: slideDownDesktop;
+          // -webkit-animation-duration: 0.6s;
+          // -webkit-animation-timing-function: ease-in;
           // transform: translateY(0);
           // -webkit-animation: slideDownDesktop 0.6s
           //   cubic-bezier(0.25, 0.8, 0.5, 1);
@@ -454,19 +454,19 @@ export default {
 @-webkit-keyframes slideDownDesktop {
   0% {
     opacity: 0;
-    -webkit-transform: scale(1);
+    -webkit-transform: translateY(-154px);
     //transform: translateY(-154px);
-    height: 1px;
+    height: 0px;
   }
   50% {
     opacity: 1;
-    -webkit-transform: scale(1.2);
+    -webkit-transform: translateY(20px);
     //transform: translateY(20px);
     height: 200px;
   }
   100% {
     opacity: 1;
-    -webkit-transform: scale(1.5);
+    -webkit-transform: translateY(0);
     //transform: translateY(0);
     height: 190px;
   }
