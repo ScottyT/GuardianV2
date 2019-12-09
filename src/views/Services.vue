b9aaa8056cfd025e112c663fdc06a5e7bd41a4da
Scott Tucker <stuck04@gmail.com>
2019-12-09 10:31:29 -0500

    fsdfsd

--- src/views/Services.vue
+++ src/views/Services.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="content services-page" v-if="loaded">
     <div class="row">
+      <div id="log"></div>
       <div class="services-page__intro">
         <h1 refs="intro" class="services-page__intro--title">Services</h1>
         <hr class="small" />
@@ -157,6 +158,16 @@ export default {
     //const intro = this.$refs.intro;
     //const timeline = new TimelineLite();
     //gsap.to(".services-page__intro--title", { rotation: 27 });
+    var card = document.querySelectorAll(".service-item__big-description");
+
+    var log = document.getElementById("log");
+    card.on("animationstart webkitAnimationStart", function() {
+      log.empty();
+      log.append("<p>Animation has started</p>");
+    });
+    card.on("animationiteration webkitAnimationIteration", function() {
+      log.append("<p>An iteration fired</p>");
+    });
   },
   methods: {
     mouseEnter: function(event) {
