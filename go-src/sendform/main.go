649275f458bf089694f133da6a0a12c1e39aa6d0
Scott Tucker <stuck04@gmail.com>
2020-08-12 14:55:09 -0400

    you can do this

--- go-src/sendform/main.go
+++ go-src/sendform/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func main() {
 	fmt.Println("You submitted a form")
+	lambda.Start(Handler)
 	// from := mail.NewEmail("Example User", "test@example.com")
 	// subject := "Sending with Twilio SendGrid is Fun"
 	// to := mail.NewEmail("Example User", "stuck04@gmail.com")
@@ -21,4 +25,11 @@ func main() {
 	// 	fmt.Println(response.Body)
 	// 	fmt.Println(response.Headers)
 	// }
+}
+
+func Handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Hello World!",
+	}, nil
 }
